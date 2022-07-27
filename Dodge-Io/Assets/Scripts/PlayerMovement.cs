83fef1baa7860c2542149e5de9bd20fe31c8e068
Çağdaş Çetin <cagdasscetin@gmail.com>
2022-07-27 15:13:07 +0300

    Walk animation added

--- Dodge-Io/Assets/Scripts/PlayerMovement.cs
+++ Dodge-Io/Assets/Scripts/PlayerMovement.cs
@@ -8,24 +8,34 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     public float turnSpeed;
 
-
+    public Animator anim;
     void FixedUpdate()
-    {
-        if (Input.GetButton("Fire1"))
-        {
-            JoyStickMovement();
-        }    
-    }
-
-    void JoyStickMovement()
     {
         float horizontal = joystick.Horizontal;
         float vertical = joystick.Vertical;
+
+        JoyStickMovement(horizontal, vertical);
+
+        //Debug.Log("horizontal " + joystick.Horizontal);
+        //Debug.Log("vertical " + joystick.Vertical);
+
+    }
+
+    void JoyStickMovement(float horizontal, float vertical)
+    {
+
         Vector3 addedPos = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
         transform.position += addedPos;
 
-        Vector3 direction = Vector3.forward * vertical + Vector3.right * horizontal;
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+        if (horizontal != 0 || vertical != 0)
+        {
+            anim.SetBool("run", true);
+            Vector3 direction = Vector3.forward * vertical + Vector3.right * horizontal;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetBool("run", false);
+        }
     }
-
 }
