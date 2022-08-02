d38ee6d62f0da0ade263e6d3646177049d808868
Çağdaş Çetin <cagdasscetin@gmail.com>
2022-08-02 11:44:44 +0300

    Boss and Player Changed with Chibi

---
+++ Dodge-Io/Assets/Scripts/Rotator.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] HitAction GameOverControllerScript;
+    int speed = 50;
+    public GameObject fountain;
+    public GameObject hitzone;
+    Vector3 sizeChange = new Vector3(1.0f, 0.0f, 0.0f );
+    int random = 0;
+    int control = 1;
+    int oldSpeed = 0;
+    int a = 0;
+    int b = 0;
+    int c = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (GameOverControllerScript.GameOverStatus)
+        {
+            speed = 0;
+        }
+        if (speed % 30 == 0 && a == b )
+        {          
+            if ( c < 7)
+            {
+                c++;
+                SizeUp();
+            }
+
+            a++;
+        }
+        fountain.transform.Rotate(Vector3.up * speed * Time.deltaTime * control);
+        random = Random.Range(1, 2001);
+        RandomCheckAndSideChange();
+        //Debug.Log(speed);
+    }
+    void waitForTime()
+    {
+        StartCoroutine("waitTime");
+    }
+    
+    IEnumerator waitTime() 
+    { 
+      yield return new WaitForSeconds(2f); 
+    }
+
+    void RandomCheckAndSideChange()
+    {
+        if (random == 500)
+        {
+            if (speed % 30 != 0 && a != b)
+            {
+                b++;
+            }
+            control = control * -1;
+            if (speed < 211)
+            {
+                speed += 10;
+            }
+            
+        }
+    }
+    void SizeUp()
+    {
+        hitzone.transform.localScale += sizeChange;
+    }
+}
